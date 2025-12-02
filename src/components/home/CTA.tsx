7e80dd4c14087a95ef1a28c0374a2eed4386d158
Adam Johnson <adam@adamjohnson.org>
2025-12-02 17:24:37 +1100

    Styling

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -1,7 +1,7 @@
 export default function CTA() {
   return (
     <div className="bg-white dark:bg-gray-900">
-      <div className="px-6 py-24 sm:py-32 lg:px-8">
+      <div className="px-6 pt-24 sm:pt-32 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
             The anonymous inbox for psychological safety.
