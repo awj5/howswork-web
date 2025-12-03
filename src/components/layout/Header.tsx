d35fb0f94d4bfa4018be8895d58bb9135df69668
Adam Johnson <adam@adamjohnson.org>
2025-12-03 22:48:41 +1100

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+      <nav aria-label="Global" className="flex items-center justify-center p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <Image src="/img/logo.svg" width={728} height={128} alt="HowsWork" className="h-7 w-auto dark:hidden" />
 
