8564c8197bc6f50ff900680531ffb11d3bdf04de
Adam Johnson <adam@adamjohnson.org>
2025-12-15 18:29:32 +1100

    Update metadata for login and not-found pages
    Added metadata with title to the login page and removed metadata from the not-found page to improve page-specific titles.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,3 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "HowsWork - Page not found",
-};
-
 export default function NotFound() {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 dark:bg-gray-900">
