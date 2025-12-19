07af4676a06a5999b28177f5548ba2058a0619b9
Adam Johnson <adam@adamjohnson.org>
2025-12-19 22:01:11 +1100

    Update pin-login.ts

--- netlify/functions/pin-login.ts
+++ netlify/functions/pin-login.ts
@@ -13,7 +13,7 @@ export const config: Config = {
 };
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // server-only
 );
 
