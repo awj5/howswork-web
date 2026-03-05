10cdb15d0012feb30bab17fdba9b7508fa0303f6
Adam Johnson <adam@adamjohnson.org>
2026-03-05 21:48:25 +1100

    Update EmptyState.tsx

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -9,7 +9,7 @@ export default function EmptyState(props: EmptyStateProps) {
   return (
     <div
       className={clsx(
-        "fixed top-1/2 left-1/2 flex h-80 w-sm -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center p-4 sm:w-md lg:top-[calc(50%+24px)]",
+        "fixed top-1/2 left-1/2 flex h-80 w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center p-4 sm:w-md lg:top-[calc(50%+24px)]",
         props.className
       )}
     >
