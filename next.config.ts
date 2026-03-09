7432a2300b0c0d5dbc669202c69dfcca31546404
Adam Johnson <adam@adamjohnson.org>
2026-03-09 16:07:15 +1100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ export default withSentryConfig(nextConfig, {
 
   org: "howswork",
 
-  project: "javascript-nextjs",
+  project: "howswork-web",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
