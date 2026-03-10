f7f277e8d9259faf3957b3aad96178f62410900c
Adam Johnson <adam@adamjohnson.org>
2026-03-10 12:13:22 +1100

    Tweaks

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
           </div>
 
           <div className="mx-auto max-w-2xl text-center">
-            <h1 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl dark:text-white">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl dark:text-white">
               A safe, anonymous way for employees to speak up
             </h1>
 
@@ -41,19 +41,6 @@ export default function Hero() {
               Give your team a trusted channel for raising workplace concerns. End-to-end encryption ensures genuine
               anonymity, supporting psychological safety and early risk detection.
             </p>
-
-            {/*<div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
-              >
-                Get started
-              </a>
-
-              <a href="#" className="text-sm/6 font-semibold text-gray-900 dark:text-white">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>*/}
           </div>
 
           <div className="mt-16 flow-root sm:mt-24">
