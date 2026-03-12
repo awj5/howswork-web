87590c46a034b9224d4085ddf42e2b9d372c4bb7
Adam Johnson <adam@adamjohnson.org>
2026-03-12 16:35:22 +1100

    Feature2

--- src/app/()/page.tsx
+++ src/app/()/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/home/Hero";
 import Newsletter from "@/components/home/Newsletter";
 import Feature1 from "@/components/home/Feature1";
+import Feature2 from "@/components/home/Feature2";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Feature1 />
+      <Feature2 />
       <Newsletter />
     </main>
   );
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -56,7 +56,7 @@ export default function Hero() {
 
           <Image
             alt="App screenshot"
-            src="/img/hero.png"
+            src="/img/concern.png"
             width={2432}
             height={1442}
             className="mt-16 rounded-md bg-gray-50 shadow-xl ring-1 ring-gray-900/10 sm:mt-24 dark:hidden"
@@ -65,7 +65,7 @@ export default function Hero() {
 
           <Image
             alt="App screenshot"
-            src="/img/hero-dark.png"
+            src="/img/concern-dark.png"
             width={2432}
             height={1442}
             className="mt-16 rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 not-dark:hidden sm:mt-24"
