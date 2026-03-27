b7632308131be7874ae1d63a5e6deefc42e62fc6
Adam Johnson <adam@adamjohnson.org>
2026-03-27 12:58:26 +1100

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,6 +4,11 @@ export function proxy(req: NextRequest) {
   const country = req.headers.get("x-nf-country") ?? "unknown";
   const res = NextResponse.next();
   res.headers.set("x-country", country);
+  const allHeaders: Record<string, string> = {};
+  req.headers.forEach((value, key) => {
+    allHeaders[key] = value;
+  });
+  console.log("All headers:", JSON.stringify(allHeaders));
   console.log("Country:", country);
   return res;
 }
