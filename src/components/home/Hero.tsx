51caf613828fe19cd8821195be2b9bbe65f1386c
Adam Johnson <adam@adamjohnson.org>
2026-03-28 18:24:14 +1100

    Country detection

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,7 +6,6 @@ import Cookies from "js-cookie";
 
 export default function Hero() {
   const country = Cookies.get("x-country");
-  console.log(country);
 
   return (
     <div className="relative isolate pt-14">
@@ -25,19 +24,21 @@ export default function Hero() {
 
       <div className="pt-24 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-400 dark:ring-white/10 dark:hover:ring-white/20">
-              Australian employers must now identify psychosocial hazards.{" "}
-              <a
-                href="https://articles.howswork.app/psychosocial-risk-at-work-what-australian-employers-need-to-know/"
-                target="_blank"
-                className="font-semibold text-indigo-600 dark:text-indigo-400"
-              >
-                <span aria-hidden="true" className="absolute inset-0" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+          {country === "AU" && (
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-400 dark:ring-white/10 dark:hover:ring-white/20">
+                Australian employers must now identify psychosocial hazards.{" "}
+                <a
+                  href="https://articles.howswork.app/psychosocial-risk-at-work-what-australian-employers-need-to-know/"
+                  target="_blank"
+                  className="font-semibold text-indigo-600 dark:text-indigo-400"
+                >
+                  <span aria-hidden="true" className="absolute inset-0" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
             </div>
-          </div>
+          )}
 
           <div className="mx-auto max-w-2xl text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl dark:text-white">
@@ -45,8 +46,8 @@ export default function Hero() {
             </h1>
 
             <p className="mt-8 text-lg font-medium text-gray-500 sm:text-xl/8 dark:text-gray-400">
-              A trusted channel for raising workplace concerns, surfacing risks early and helping reduce exposure to
-              psychological injury claims and compliance failures.
+              A trusted psychological health and safety platform to raise workplace concerns, surfacing risks early with
+              a clear record of action taken.
             </p>
 
             <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -54,7 +55,7 @@ export default function Hero() {
                 href="/contact/demo"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
               >
-                Book a demo
+                {/* Get started */}Request a demo
               </Link>
 
               <a
