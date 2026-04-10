8f228ca084a8300fb02ff1a20f467eefa9784753
Adam Johnson <adam@adamjohnson.org>
2026-04-10 14:51:35 +1000

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -134,7 +134,7 @@ export default function Header() {
                   href="https://admin.howswork.app"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-white/5"
                 >
-                  Employer log in
+                  Log in
                 </a>
               </div>
             </div>
