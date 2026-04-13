1f2d3498fe8a09bdbf38302c5aa079bd0f907a4f
Adam Johnson <adam@adamjohnson.org>
2026-04-13 16:59:15 +1000

    Update page.tsx

--- src/app/()/pricing/page.tsx
+++ src/app/()/pricing/page.tsx
@@ -12,6 +12,7 @@ const tiers = [
   {
     name: "Starter",
     id: "tier-starter",
+    planID: 1,
     href: "/contact/demo",
     price: { monthly: "$0", annually: "$0" },
     audPrice: { monthly: "$0", annually: "$0" },
@@ -22,6 +23,7 @@ const tiers = [
   {
     name: "Team",
     id: "tier-team",
+    planID: 2,
     href: "/contact/demo",
     price: { monthly: "$99", annually: "$990" },
     audPrice: { monthly: "$149", annually: "$1,490" },
@@ -39,6 +41,7 @@ const tiers = [
   {
     name: "Business",
     id: "tier-business",
+    planID: 3,
     href: "/contact/demo",
     price: { monthly: "$249", annually: "$2,490" },
     audPrice: { monthly: "$349", annually: "$3,490" },
@@ -57,6 +60,7 @@ const tiers = [
   {
     name: "Enterprise",
     id: "tier-enterprise",
+    planID: 4,
     href: "/contact/sales",
     price: { monthly: "$0", annually: "$0" },
     audPrice: { monthly: "$0", annually: "$0" },
@@ -180,7 +184,7 @@ function PricingContent() {
                   href={
                     !dashboard || tier.id === "tier-enterprise"
                       ? tier.href
-                      : `${dashboardURL}/upgrade?company=${dashboard}&plan=${tier.id}&billing=monthly`
+                      : `${dashboardURL}/upgrade?company=${dashboard}&plan=${tier.planID}&billing=monthly`
                   }
                   aria-describedby={tier.id}
                   className="mt-6 block w-full rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 inset-ring-1 inset-ring-indigo-200 group-not-has-[[name=frequency][value=monthly]:checked]/tiers:hidden group-data-featured/tier:bg-indigo-600 group-data-featured/tier:text-white group-data-featured/tier:shadow-xs group-data-featured/tier:inset-ring-0 hover:inset-ring-indigo-300 group-data-featured/tier:hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-white/10 dark:text-white dark:inset-ring dark:inset-ring-white/5 dark:group-data-featured/tier:bg-indigo-500 dark:group-data-featured/tier:shadow-none dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:group-data-featured/tier:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500 dark:group-not-data-featured/tier:focus-visible:outline-white/75"
@@ -192,7 +196,7 @@ function PricingContent() {
                   href={
                     !dashboard || tier.id === "tier-enterprise"
                       ? tier.href
-                      : `${dashboardURL}/upgrade?company=${dashboard}&plan=${tier.id}&billing=annually`
+                      : `${dashboardURL}/upgrade?company=${dashboard}&plan=${tier.planID}&billing=annually`
                   }
                   aria-describedby={tier.id}
                   className="mt-6 block w-full rounded-md px-3 py-2 text-center text-sm/6 font-semibold text-indigo-600 inset-ring-1 inset-ring-indigo-200 group-not-has-[[name=frequency][value=annually]:checked]/tiers:hidden group-data-featured/tier:bg-indigo-600 group-data-featured/tier:text-white group-data-featured/tier:shadow-xs group-data-featured/tier:inset-ring-0 hover:inset-ring-indigo-300 group-data-featured/tier:hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-white/10 dark:text-white dark:inset-ring dark:inset-ring-white/5 dark:group-data-featured/tier:bg-indigo-500 dark:group-data-featured/tier:shadow-none dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:group-data-featured/tier:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500 dark:group-not-data-featured/tier:focus-visible:outline-white/75"
